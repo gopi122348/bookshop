34caffa4abab66e0cff5aab47155fe95a57cd281
EC2 Default User <ec2-user@ip-172-31-1-117.ec2.internal>
2026-03-27 21:32:34 +0000

    Handle permission error on db cleanup

--- cleanup_db.py
+++ cleanup_db.py
@@ -1,5 +1,18 @@
-import os
+import os, sqlite3
+
 db = '/tmp/db.sqlite3'
-if os.path.exists(db):
-    os.remove(db)
-    print("Removed old db")
+try:
+    if os.path.exists(db):
+        os.remove(db)
+        print("Removed old db")
+except PermissionError:
+    # Can't delete, so wipe all tables instead
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables = cursor.fetchall()
+    for (table,) in tables:
+        conn.execute(f'DROP TABLE IF EXISTS "{table}"')
+    conn.commit()
+    conn.close()
+    print("Wiped all tables in existing db")
