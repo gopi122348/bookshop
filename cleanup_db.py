1352cc39d02c3528977afc353326aa7190d422dc
EC2 Default User <ec2-user@ip-172-31-1-117.ec2.internal>
2026-03-27 21:26:35 +0000

    Wipe entire tmp db instead of individual tables

--- cleanup_db.py
+++ cleanup_db.py
@@ -1,9 +1,5 @@
-import sqlite3, os
+import os
 db = '/tmp/db.sqlite3'
 if os.path.exists(db):
-    conn = sqlite3.connect(db)
-    conn.execute('DROP TABLE IF EXISTS books_order')
-    conn.execute('DROP TABLE IF EXISTS books_orderitem')
-    conn.commit()
-    conn.close()
-    print("Cleaned up bad tables")
+    os.remove(db)
+    print("Removed old db")
