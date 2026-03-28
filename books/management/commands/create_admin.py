ffc8d48dcfd84eca53b6440132c5fa0600767b93
EC2 Default User <ec2-user@ip-172-31-1-117.ec2.internal>
2026-03-28 20:10:53 +0000

    Seed sample books on deploy

--- books/management/commands/create_admin.py
+++ books/management/commands/create_admin.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from books.models import Book
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
@@ -7,5 +8,9 @@ class Command(BaseCommand):
         if not U.objects.filter(username='admin').exists():
             U.objects.create_superuser('admin', 'admin@example.com', 'Admin1234!')
             self.stdout.write('Superuser created.')
-        else:
-            self.stdout.write('Superuser already exists.')
+
+        if Book.objects.count() == 0:
+            Book.objects.create(title='The Midnight Library', author='Matt Haig', price=12.99, stock=10, genre='FIC', isbn='9780525559474')
+            Book.objects.create(title='Atomic Habits', author='James Clear', price=14.99, stock=15, genre='NON', isbn='9780735211292')
+            Book.objects.create(title='1984', author='George Orwell', price=9.99, stock=20, genre='FIC', isbn='9780451524935')
+            self.stdout.write('Sample books created.')
