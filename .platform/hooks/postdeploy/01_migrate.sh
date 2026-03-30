368115db668a2b06f2987f3f57718b0e984e75d0
EC2 Default User <ec2-user@ip-172-31-1-117.ec2.internal>
2026-03-30 22:36:14 +0000

    Reset DB and admin password on deploy

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-
+set -e
+source /var/app/venv/*/bin/activate
 cd /var/app/current
 
-/var/app/venv/staging-LQM1lest/bin/python manage.py migrate --noinput
+rm -f /var/app/current/db.sqlite3
+
+python manage.py migrate --noinput
+
+python manage.py shell -c "
+from django.contrib.auth.models import User
+u, created = User.objects.get_or_create(username='admin')
+u.set_password('Admin1234')
+u.is_superuser = True
+u.is_staff = True
+u.save()
+print('Admin ready')
+"
